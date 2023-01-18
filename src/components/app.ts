29b504b565f519c8f85f18198a139c1589b37305
Eduardaskuliesa <eduardas2000@mail.ru>
2023-01-18 20:26:25 +0200

    feat(5) completed task 3

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,8 +1,17 @@
+import CarsCollection from '../helpers/cars-collection';
+import brands from '../data/brands';
+import cars from '../data/cars';
+import models from '../data/models';
+
 class App {
     private htmlElement: HTMLElement;
 
+    private carsColletion: CarsCollection;
+
     constructor(selector: string) {
       const foundElement = document.querySelector<HTMLElement>(selector);
+      this.carsColletion = new CarsCollection({ cars, brands, models });
+      console.log(this.carsColletion);
 
       if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
 
@@ -17,5 +26,4 @@ class App {
       this.htmlElement.append(container);
     };
   }
-
   export default App;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,4 @@
-import CarsCollection from './helpers/cars-collection';
-import brands from './data/brands';
-import cars from './data/cars';
-import models from './data/models';
+import App from './components/app';
 
-const carsCollection = new CarsCollection({
-    cars,
-    models,
-    brands,
-});
-console.log(carsCollection);
+const app = new App('#root');
+app.initialize();
